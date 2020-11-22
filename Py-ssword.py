baaba43b8e382e7716f69130cf390004ac2d9cdd
Arushi A <arushiagarwal14@gmail.com>
2020-11-23 00:02:19 +0530

    Update Py-ssword.py

--- Py-ssword.py
+++ Py-ssword.py
@@ -3,10 +3,12 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the Py-ssword Suggestor!")
+print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+level = input("What type of password do you want? Secure, or very Secure? (S or VS)\n")
 
 total_len = nr_letters + nr_symbols + nr_numbers
 what_type = random.randint(0,2)
@@ -27,4 +29,7 @@ better = []
 for i in password:
     better.append(password[random.randint(0, len(password)-1)])
     
-print(''.join(better))
+if level == 'S':
+    print(password)
+else:
+    print(''.join(better))
